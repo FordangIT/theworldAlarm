ce50578e871430c851faac30e2137929c634d7a5
FordangIT <fordang0819@gmail.com>
2023-08-26 17:50:09 +0900

    [컴포넌트]모달창 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
+import GoAbout from "@/components/navbar";
+import Modal_Alarm from "@/components/modal_alarm";
+import { faShareFromSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSquareMinus,
+  faSquarePlus,
+} from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Alarm() {
   return (
-    <main>
-      <div>
-        <p className="text-3xl">2023년 8월 25일 금요일</p>
-      </div>
-      <div>
-        <span className="text-5xl">오후</span>
-        <span className="text-9xl">4:52:50</span>
+    <main className="bg-orange-50 text-center">
+      <FontAwesomeIcon icon={faShareFromSquare} />
+      <FontAwesomeIcon icon={faSquareMinus} />
+      <FontAwesomeIcon icon={faSquarePlus} />
+      <div className="py-40">
+        <div className="mb-4">
+          <p className="text-3xl text-gray-500 ml-20">2023년 8월 25일 금요일</p>
+        </div>
+        <div>
+          <span className="text-5xl mr-4">오후</span>
+          <span className="text-9xl">4:52:50</span>
+        </div>
+        <Modal_Alarm />
+        <GoAbout />
       </div>
     </main>
   );
