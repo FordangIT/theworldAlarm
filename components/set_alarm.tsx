0d74a80192b69a46b79ad0c57fed9334e1859bf4
FordangIT <fordang0819@gmail.com>
2023-08-29 17:47:09 +0900

    알람 설정

--- components/set_alarm.tsx
+++ components/set_alarm.tsx
@@ -1,8 +1,20 @@
-export function Set_Alarm() {
+import React, { PropsWithChildren } from "react";
+interface ModalDefaultType {
+  clickModal: () => void;
+}
+export function Set_Alarm({ clickModal }: PropsWithChildren<ModalDefaultType>) {
   return (
-    <div className="fixed w-full h-full items-center justify-center">
-      <div className="fixed top-0 left-0 w-screen h-screen bg-gray-100 -z-10 opacity-75 "></div>
-      <div className="w-56 h-56 bg-gray-600 z-30 relative items-center justify-center"></div>
+    <div className="fixed w-full h-full items-center justify-center ">
+      <div className="mx-auto w-96 h-96 bg-white z-30 flex flex-col items-center border-none "></div>
+      <div
+        className="fixed top-0 left-0 w-screen h-screen bg-black -z-10 opacity-30 "
+        onClick={(e: React.MouseEvent) => {
+          e.preventDefault();
+          if (clickModal) {
+            clickModal();
+          }
+        }}
+      ></div>
     </div>
   );
 }
