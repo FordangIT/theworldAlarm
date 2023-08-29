e339d03a52636f1ec599610cc1a1163658df992e
FordangIT <fordang0819@gmail.com>
2023-08-29 17:48:17 +0900

    기본 홈 디자인 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,37 @@
 import GoAbout from "@/components/navbar";
 import Modal_Alarm from "@/components/modal_alarm";
-import { faShareFromSquare } from "@fortawesome/free-solid-svg-icons";
-import {
-  faSquareMinus,
-  faSquarePlus,
-} from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Icons from "@/components/icons";
+import dayjs from "dayjs";
+dayjs.locale("ko");
+import { useState, useEffect } from "react";
 export default function Alarm() {
+  let year: string = dayjs().format("YYYY");
+  let month: string = dayjs().format("MM");
+  let day: string = dayjs().format("DD");
+  let week: string = dayjs().format("dddd");
+  let hour: string = dayjs().format("HH");
+  let minute: string = dayjs().format("mm");
+  let second: string = dayjs().format("ss");
+  const [nowTime, setNowTime] = useState(Date.now());
+
   return (
     <main className="bg-orange-50 text-center">
-      <FontAwesomeIcon icon={faShareFromSquare} />
-      <FontAwesomeIcon icon={faSquareMinus} />
-      <FontAwesomeIcon icon={faSquarePlus} />
+      <div className="flex justify-end items-start pt-8 pr-6">
+        <Icons />
+      </div>
       <div className="py-40">
         <div className="mb-4">
-          <p className="text-3xl text-gray-500 ml-20">2023년 8월 25일 금요일</p>
+          <p className="text-3xl text-gray-500 ml-20">
+            {year}년 {month}월 {day}일 {week}
+          </p>
         </div>
         <div>
           <span className="text-5xl mr-4">오후</span>
-          <span className="text-9xl">4:52:50</span>
+          <span className="text-9xl">
+            {hour}:{minute}:{second}
+          </span>
+          <Modal_Alarm />
         </div>
-        <Modal_Alarm />
-        <GoAbout />
       </div>
     </main>
   );
