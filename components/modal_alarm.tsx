5269df995277b9cf3fef291f8351ad2a8c8ec081
FordangIT <fordang0819@gmail.com>
2023-08-29 17:46:51 +0900

    [알람 모달창]

--- components/modal_alarm.tsx
+++ components/modal_alarm.tsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { Set_Alarm } from "./set_alarm";
 export default function Modal_Alarm() {
   const [showModal, setShowModal] = useState(false);
-  const clickModal = () => {
+  const clickModal = useCallback(() => {
     setShowModal(!showModal);
-  };
+  }, [showModal]);
   return (
-    <div>
+    <div className="">
+      {showModal && <Set_Alarm clickModal={clickModal}></Set_Alarm>}
       <button
         onClick={clickModal}
         className="my-8 ml-16 bg-blue-600 hover:bg-blue-400 text-white font-bold py-2 px-8 rounded-full"
       >
         Button
       </button>
-      {showModal && <Set_Alarm />}
     </div>
   );
 }
