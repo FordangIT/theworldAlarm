44c20891d3f2ea436a2f289179ff5283577e7501
FordangIT <fordang0819@gmail.com>
2023-09-16 19:48:44 +0900

    [FE]스톱워치 기본 기능 설정

--- pages/stopwatchpage.tsx
+++ pages/stopwatchpage.tsx
@@ -3,12 +3,44 @@ import Icons from "@/components/Icons";
 import { useRef, useState, useEffect } from "react";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
+export const formatTime = (timeInMillis: number): string => {
+  const hours = Math.floor(timeInMillis / 3600000);
+  const minutes = Math.floor((timeInMillis % 360000) / 60000);
+  const seconds = Math.floor((timeInMillis % 60000) / 1000);
+  const formattedTime = `${hours}:${minutes < 10 ? "0" : ""}${minutes}:${
+    seconds < 10 ? "0" : ""
+  }${seconds}`;
+  return formattedTime;
+};
 export default function StopWatch() {
-  const timerIdRef = useRef(0);
-  const [count, setCount] = useState(0);
-
-  const startHandler = () => {
-0
+  const [running, setRunning] = useState<boolean>(false);
+  const [time, setTime] = useState<number>(0);
+  const [laps, setLaps] = useState<number[]>([]);
+  const intervalRef = useRef<NodeJS.Timeout | undefined>(undefined);
+  const startStopWatch = () => {
+    if (!running) {
+      intervalRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+      setRunning(!running);
+    } else {
+      clearInterval(intervalRef.current);
+      setRunning(!running);
+    }
+  };
+  const resetStopWatch = () => {
+    clearInterval(intervalRef.current);
+    setTime(0);
+    setLaps([]);
+    setRunning(false);
+  };
+  const recordLap = () => {
+    const newLap = {
+      id: laps.length === 0 ? 1 : laps[laps.length - 1].id + 1,
+      lap: time,
+    };
+    setLaps((prevLaps) => [...prevLaps, newLap]);
+  };
   return (
     <div>
       <Header />
@@ -20,12 +52,18 @@ export default function StopWatch() {
           <div className="flex justify-end items-start pt-8 pr-3">
             <Icons />
           </div>
-          <div className="py-40 text-center pr-4">
-            <div>Timer : {count}</div>
+          {/*여기부터가 스톱워치 만드는 부분*/}
+          <div gap={12}>
+            <div>{formatTime(time)}</div>
             <div>
-              <button onClick={startHandler}>start</button>
-              <button onClick={stopHandler}>stop</button>
+              <button onClick={startStopWatch}>
+                {running ? "Stop" : "Start"}
+              </button>
+
+              <button onClick={resetStopWatch}>Reset</button>
             </div>
+            <div laps={laps} setLaps={setLaps}></div>
+            {running && <button onClick={recordLap}>Lap</button>}
           </div>
         </div>
       </div>
