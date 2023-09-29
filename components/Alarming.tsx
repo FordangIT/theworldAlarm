9c078b8456072ee13437376d54894b84d593b57e
FordangIT <fordang0819@gmail.com>
2023-09-29 14:36:14 +0900

    [FE]redux 상태 가져오는 코드 완료

--- components/Alarming.tsx
+++ components/Alarming.tsx
@@ -1,18 +1,30 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import alarmSounds from "../public/alarmSounds.mp3";
 import { useAppSelector } from "@/hook/reduxHooks";
+import { RootState } from "@/redux/store";
 //set alarm에서 선택한 시간 값을 받아온다. 그 값이 현재 시간과 일치할 때 알람 소리를 울린다. !
 export default function Alarming() {
-  const sound = new Audio(alarmSounds);
+  const sound = useRef<false | HTMLAudioElement>(
+    typeof Audio !== "undefined" && new Audio(alarmSounds)
+  );
   const [nowTime, setNowTime] = useState(new Date());
   const ampmStand = nowTime.getHours() > 12 ? `오후` : `오전`;
   const hourStand = nowTime.getHours();
   const minutesStand = nowTime.getMinutes();
   const [alarmState, setAlarmState] = useState<HTMLAudioElement>();
-  const alarmAmpm = useAppSelector((state) => state.ampm);
-  const soundHandler = () => {
-    alarmState && sound.play();
+  const alarmAmpm = useAppSelector(
+    (state: RootState) => state.alarmTimeSave.ampm
+  );
+  const alarmHour = useAppSelector(
+    (state: RootState) => state.alarmTimeSave.hour
+  );
+  const alarmMinutes = useAppSelector((state) => state.alarmTimeSave.minutes);
+  const soundPlayHandler = () => {
+    sound.play();
+  };
+  const soundStopHandler = () => {
+    console.log(sound.play());
   };
   useEffect(() => {
     const id = setInterval(() => {
@@ -21,24 +33,20 @@ export default function Alarming() {
     return () => clearInterval(id);
   }, []);
 
-  useEffect(() => {
-    setAlarmState(new Audio(alarmSounds));
-  }, []);
-  const checking = () => {
-    console.log(alarmAmpm);
-  };
+  // useEffect(() => {
+  //   setAlarmState(new Audio(alarmSounds));
+  // }, []);
+
   return (
     <div>
       <div>
-        {/* {ampm === ampmStand && hour === hourStand && minutes === minutesStand
-          ? { soundHandler }
-          : undefined} */}
-        <button onClick={soundHandler} className="w-8 h-5 bg-black">
-          {" "}
+        <button onClick={soundPlayHandler} className="w-8 h-5 bg-black">
           누르면 소리가 나나?
         </button>
-        <button onClick={checking}>데이터 가져오기 확인</button>
-        {/* {ampmStand === && hourStand === && minutesStand === ? soundHandler() : } */}
+        {/* {ampmStand === alarmAmpm &&
+          hourStand === alarmHour &&
+          minutesStand === alarmMinutes &&
+          ? soundPlayHandler() : } */}
       </div>
     </div>
   );
